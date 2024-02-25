e06d44237c9151df9bf72ccfa89b9c91bc0ffad5
Ricwolf19 <rhtc19@gmail.com>
2024-02-25 04:29:24 -0600

    Fix errors finally

--- src/Components/Alert.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from "react-native";
-
-export function Alert({ mesagge }: any) {
-    return (
-        <View className="bg-red-100 border-red-400 text-red-700 px-4 py-3 rounded relative mb-2 text-center">
-            <Text className="sm:inline-block">{mesagge}</Text>
-        </View>
-    )
-}
---
+++ src/app/WelcomeScreen.tsx
@@ -0,0 +1,49 @@
+import { View, Image, Text } from "react-native";
+import React, { useEffect } from 'react'
+import { StatusBar } from "expo-status-bar";
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+import Animated, {
+    useSharedValue,
+    withSpring,
+  } from 'react-native-reanimated';
+import { router } from "expo-router";
+
+export default function WelcomeScreen() {
+    const ring1padding = useSharedValue(0)
+    const ring2padding = useSharedValue(0)
+
+    useEffect(() => {
+        ring1padding.value = 0;
+        ring2padding.value = 0;
+        setTimeout(() => ring1padding.value = withSpring(ring1padding.value+hp(5)), 100);
+        setTimeout(() => ring2padding.value = withSpring(ring2padding.value+hp(3)), 300);
+
+        setTimeout(() => router.replace('/'), 2200)
+    }, [])
+
+    return (
+        <View className="flex-1 justify-center items-center space-y-10 bg-blue-200/50">
+            <StatusBar style="light" />
+
+            {/* Logo */}
+            <Animated.View className="bg-blue-300/50 rounded-full" style={{padding: ring2padding}}>
+                <Animated.View className="bg-white/20 rounded-full" style={{padding: ring1padding}}>
+                    <Image source={require('../.././public/logo-rbg.png')}
+                        style={{ width: hp(13), height: hp(10) }} />
+                </Animated.View>
+            </Animated.View>
+
+            <View style={{padding: hp(1)}}></View>
+
+            {/* Title */}
+            <View className="flex items-center space-y-2">
+                <Text className="font-bold text-red-700 tracking-widest" style={{fontSize: hp(6)}}>
+                    Vita Link
+                </Text>
+                <Text className="font-medium text-blue-800 tracking-widest" style={{fontSize: hp(2)}}>
+                    Smart Monitoring, Real Results
+                </Text>
+            </View>
+        </View>
+    )
+}

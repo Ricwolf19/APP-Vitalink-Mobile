5ea3d6c2a370cc83de8cdead99c670f55967599a
Ricwolf19 <rhtc19@gmail.com>
2024-02-27 17:58:07 -0600

    Firebase creadentials on .env

--- Firebase.tsx
+++ Firebase.tsx
@@ -7,14 +7,14 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA_jmBWhuWMCcqq_1Lf0LF1R3AviWgSm_I",
-  authDomain: "vitalinkhub.firebaseapp.com",
-  databaseURL: "https://vitalinkhub-default-rtdb.firebaseio.com",
-  projectId: "vitalinkhub",
-  storageBucket: "vitalinkhub.appspot.com",
-  messagingSenderId: "632585986679",
-  appId: "1:632585986679:web:f736896eaf39755a969c72",
-  measurementId: "G-0T1VS44H7X"
+  apiKey: process.env.VITE_FIREBASE_API_KEY,
+  authDomain: process.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: process.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.VITE_FIREBASE_APP_ID,
+  measurementId: process.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 

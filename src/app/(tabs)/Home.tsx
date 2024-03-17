87d3ed706da983c1cd957d8c8a5249dc39364da2
Antonio Rosales <jose.rosales.mald@gmail.com>
2024-03-16 19:52:24 -0600

    component disabled for now

--- src/app/(tabs)/Home.tsx
+++ src/app/(tabs)/Home.tsx
@@ -1,28 +1,26 @@
-import React, { useState } from 'react';
-import { View, Text, Button, ActivityIndicator } from 'react-native';
-import { logOut, useAccountData } from '../../context/authContext';
-import Spinner from '@/components/spinner';
+// import React, { useState } from 'react';
+// import { View, Text, Button, ActivityIndicator } from 'react-native';
+// import Spinner from '@/components/spinner';
 
-export default function Home() {
-  const { accountData } = useAccountData();
-  console.log(accountData);
+// export default function Home() {
+//   const { accountData } = useAccountData();
+//   console.log(accountData);
   
 
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <View>
-        {accountData ? (
-          <View>
-            <Text>Welcome, {accountData.userName}</Text>
-            <Text>Email: {accountData.email}</Text>
-            <Text>Rol: {accountData.rol}</Text>
-            <Text>{accountData.rol}</Text>
-          </View>
-        ) : (
-          <Spinner/>
-        )}
-        <Button title="Log Out" onPress={() => logOut()} />
-      </View>
-    </View>
-  );
-}
+//   return (
+//     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+//       <View>
+//         {accountData ? (
+//           <View>
+//             <Text>Welcome, {accountData.userName}</Text>
+//             <Text>Email: {accountData.email}</Text>
+//             <Text>Rol: {accountData.rol}</Text>
+//             <Text>{accountData.rol}</Text>
+//           </View>
+//         ) : (
+//           <Spinner/>
+//         )}
+//       </View>
+//     </View>
+//   );
+// }
